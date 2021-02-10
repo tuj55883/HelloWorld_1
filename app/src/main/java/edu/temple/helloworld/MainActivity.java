3f75d59b31d62ca04c1547288a045152980b01de
tuj55883 <78548481+tuj55883@users.noreply.github.com>
2021-02-10 17:13:33 -0500

    Added a button that, when pressed, changes the text.

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,12 +3,31 @@
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity {
+
+     View button;
+     TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.myButton);
+        textView = findViewById(R.id.textView);
+
+        View.OnClickListener onClickListener = new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                textView.setText("You have clicked the button");
+            }
+        };
+
+        button.setOnClickListener(onClickListener);
+        
     }
 }
